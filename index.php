5dca09da77a3a048f12e05a4750d9d810c353066
“Satomi <satomi2458@yahoo.co.jp>
2020-10-03 13:26:28 +0900

    progate php1

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Progate</title>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+	<body>
+		<?php echo 'Hello, world'; ?>
+		<br>
+		<?php echo 7 * 2; ?>
+		<br>
+		<?php echo 8 % 3; ?>
+		<br>
+		<?php echo 5 + 7; ?>
+		<br>
+		<?php echo '5 + 7'; ?>
+		<br>
+		<?php
+		 $fruit = 'りんご';
+		 echo $fruit;
+		?>
+		<br>
+		<?php
+		 $sum = 8 + 9;
+		 echo $sum;
+		?>
+		<br>
+		<?php
+		 $x = 5;
+		 $y = 2;
+		 $a = 8;
+		 $b = 4;
+		?>
+		<br>
+		<?php
+		 $x += 10;
+		 echo $x
+		?>
+		<br>
+		<?php
+		 $y *= 5;
+		 echo $y;
+		?>
+		<br>
+		<?php
+		 $a++;
+		 echo $a;
+		?>
+		<br>
+		<?php
+		 $b--;
+		 echo $b;
+		?>
+
+
+	</body>
+</html>
