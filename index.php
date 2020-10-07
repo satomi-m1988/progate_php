92c5ed9d3382502bc464fb5bbd30b0d5fe02d77c
“Satomi <satomi2458@yahoo.co.jp>
2020-10-07 20:21:03 +0900

    progate php2

--- index.php
+++ index.php
@@ -52,6 +52,61 @@
 		 $b--;
 		 echo $b;
 		?>
+		<br>
+		<?php
+		 $name = 'にんじゃわんこ';
+		 echo "こんにちは！${name}";
+		?><br>
+		<?php
+		 $x = 99 * 99;
+		 $y = 77 * 77;
+
+		 if($x > 9800){
+		 	echo '変数xは9800より大きいです。';
+		 }
+		 if($y > 6000){
+		 	echo '変数yは6000より大きいです。';
+		 }
+		?><br>
+		<?php
+		 $age = 32;
+		 if($age >= 30){
+		 	echo 'あなたは30歳以上です。';
+		 }else{
+		 	echo 'あなたは30歳未満です。';
+		 }
+		?><br>
+		<?php
+		 $x = 1071;
+
+		 if($x % 3 == 0 && $x % 7 == 0){
+		 	echo 'xは3の倍数かつ7の倍数です。';
+		 }elseif($x % 3 == 0){
+		 	echo 'xは3の倍数ですが7の倍数ではありません。';
+		 }elseif($x % 7 == 0){
+		 	echo 'xは7の倍数ですが3の倍数ではありません。';
+		 }else{
+		 	echo 'xは7の倍数でも3の倍数でもありません。';
+		 }
+		?><br>
+		<?php
+		 $num = 7;
+		 $remainder = $num % 3;
+
+		 switch($remainder){
+		 	case 0:
+		 	 echo '大吉です。';
+		 	break;
+		 	case 1:
+		 	 echo '中吉です。';
+		 	break;
+		 	case 2:
+		 	 echo '小吉です。';
+		 	break;
+		 	default:
+		 	 echo '凶です。';
+		 	break;
+		 }?>
 
 
 	</body>
